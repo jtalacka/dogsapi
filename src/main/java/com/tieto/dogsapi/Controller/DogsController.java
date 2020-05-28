50b78ca6f12e548adb407953a1577ceab6eb614e
jtalacka <julius.talacka@gmail.com>
2020-05-28 16:11:56 +0300

    set database for heroku

--- src/main/java/com/tieto/dogsapi/Controller/DogsController.java
+++ src/main/java/com/tieto/dogsapi/Controller/DogsController.java
@@ -4,6 +4,7 @@ import com.tieto.dogsapi.Service.AnimalDAO;
 import com.tieto.dogsapi.model.Animal;
 import com.tieto.dogsapi.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class DogsController {
 
         return animals;
     }
+    @CrossOrigin
     @GetMapping("/get")
     public Animal getOne(){
 
